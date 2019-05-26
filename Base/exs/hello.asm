5551c6fcb151bf0f1d75279f60d61d05d27cca53
MasterWigu <moreira.oliveira@live.com.pt>
2019-05-26 17:24:41 +0100

    asfljasdbfl

---
+++ Base/exs/hello.asm
@@ -0,0 +1,141 @@
+; TEXT
+segment	.text
+; ALIGN
+align	4
+; GLOBL
+global	$_entry:function
+; LABEL
+$_entry:
+; ENTER
+	push	ebp
+	mov	ebp, esp
+	sub	esp, 8
+; IMM
+	push	dword 20
+; COPY
+	push	dword [esp]
+; LOCAL
+	lea	eax, [ebp+-8]
+	push	eax
+; STORE
+	pop	ecx
+	pop	eax
+	mov	[ecx], eax
+; TRASH
+	add	esp, 4
+; LABEL
+$_i1:
+; LOCAL
+	lea	eax, [ebp+-8]
+	push	eax
+; COPY
+	push	dword [esp]
+; LOAD
+	pop	eax
+	push	dword [eax]
+; SWAP
+	pop	eax
+	pop	ecx
+	push	eax
+	mov	eax, ecx
+	push	eax
+; INCR
+	pop	eax
+	add	dword [eax], 1
+; TRASH
+	add	esp, 4
+; LOCAL
+	lea	eax, [ebp+-8]
+	push	eax
+; LOAD
+	pop	eax
+	push	dword [eax]
+; IMM
+	push	dword 30
+; LT
+	pop	eax
+	xor	ecx, ecx
+	cmp	[esp], eax
+	setl	cl
+	mov	[esp], ecx
+; JNZ
+	pop	eax
+	cmp	eax, byte 0
+	jne	near $_i1
+; LABEL
+$_i2:
+; DATA
+segment	.data
+; ALIGN
+align	4
+; LABEL
+$_i3:
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x6C
+; CHAR
+	db	0xC3
+; CHAR
+	db	0xA1
+; CHAR
+	db	0x20
+; CHAR
+	db	0x70
+; CHAR
+	db	0x65
+; CHAR
+	db	0x73
+; CHAR
+	db	0x73
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x61
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x21
+; CHAR
+	db	0x0A
+; CHAR
+	db	0x00
+; TEXT
+segment	.text
+; ADDR
+	push	dword $_i3
+; CALL
+	call	$_prints
+; TRASH
+	add	esp, 4
+; PUSH
+	push	eax
+; TRASH
+	add	esp, 4
+; IMM
+	push	dword 0
+; COPY
+	push	dword [esp]
+; LOCAL
+	lea	eax, [ebp+-4]
+	push	eax
+; STORE
+	pop	ecx
+	pop	eax
+	mov	[ecx], eax
+; TRASH
+	add	esp, 4
+; LOCAL
+	lea	eax, [ebp+-4]
+	push	eax
+; LOAD
+	pop	eax
+	push	dword [eax]
+; POP
+	pop	eax
+; LEAVE
+	leave
+; RET
+	ret
+; EXTRN
+extern	$_prints
